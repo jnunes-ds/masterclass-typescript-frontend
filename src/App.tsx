df20391f616b22de5072c1643b683dedd0f9bea5
Junior Nunes <denisonprobable13@gmail.com>
2021-05-03 17:19:55 -0300

    Determining the type of data that will be consumed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+
+interface IUser{
+  name: string;
+  email: string;
+}
 
 function App() {
+  const [users, setUsers]= useState<IUser[]>([]);
+
+  useEffect(() => {
+    api.get<IUser[]>('/users').then(response => {
+      setUsers(response.data);
+    })
+  }, []);
+
   return (
     <div className="App">
-      
+      { users.map(user => <p>{  }</p>) }
     </div>
   );
 }
